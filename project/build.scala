5c9c71e05085f2a63e49bc681c20a2bd168a631c
Alexandre Bertails <bertails@w3.org>
2012-05-22 11:04:47 -0400

    a leaner build configuration

--- project/build.scala
+++ project/build.scala
@@ -5,18 +5,12 @@ import WebPlugin._
 import PluginKeys._
 
 /**
- * build configuration for rdfvalidator-ng
- *
- * it relies on:
- * - xsbt-web-plugin. see https://github.com/siasia/xsbt-web-plugin
- * - Jena 2.9.0
- * - Jetty 8.x and Java Servlet 2.5
- * - slf4j for logging
+ * build configuration for CSS Validator Standalone
  */
-object RDFValidatorNGBuild extends Build {
+object CSSValidator extends Build {
 
-  lazy val rdfValidator = Project(
-    id = "rdf-validator-ng",
+  lazy val cssValidator = Project(
+    id = "css-validator-standalone",
     base = file("."),
     settings = Defaults.defaultSettings ++ webSettings ++ Seq(
       organization := "org.w3",
@@ -26,9 +20,6 @@ object RDFValidatorNGBuild extends Build {
       resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
       resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
       resolvers += "apache-repo-releases" at "http://repository.apache.org/content/repositories/releases/",
-      libraryDependencies += "javax.mail" % "mail" % "1.4.1",
-      libraryDependencies += "org.apache.jena" % "jena-arq" % "2.9.0-incubating",
-      libraryDependencies += "com.fasterxml" % "aalto-xml" % "0.9.7",
       libraryDependencies += "org.eclipse.jetty" % "jetty-webapp" % "8.0.1.v20110908" % "compile,container",
       libraryDependencies += "javax.servlet" % "servlet-api" % "2.5" % "provided",
       libraryDependencies += "org.slf4j" % "slf4j-log4j12" % "1.6.4"
